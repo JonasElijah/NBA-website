a86b6e7665f4eca3f6281957802da1ca888fc8d0
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-08-24 14:45:21 -0500

    removed app.css import from app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer'; 
 import Home from './routes/Home/home';
