21caca64499c45555750eae3cccc82999b04f251
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-08-24 15:05:48 -0500

    add a bunch of headers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,10 @@ function App() {
     <>
       <Router>
         <Header />
+        <Header />
+        <Header />
+        <Header />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
