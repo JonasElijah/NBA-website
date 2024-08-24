b9a419f023a0551b385ff0c8ce5f5be60bfbe60f
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-08-24 15:33:11 -0500

    updated header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,9 +8,21 @@ function Header() {
       <header>
         <Link to="/" className="logo"><img src={logo} alt="React" />NBA Website</Link>
         <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/books">Books</NavLink>
-          <NavLink to="/about">About</NavLink>
+          <NavLink to="/players">players</NavLink>
+          <select name="teams" id="teams">
+            <option value="gsw">Warriors</option>
+            <option value="lal">Lakers</option>
+            <option value="sa">Spurs</option>
+            <option value="dal">Mavericks</option>
+          </select>
+          <select name="seasons" id="seasons">
+            <option value="2324">2023-2024</option>
+            <option value="2223">2022-2023</option>
+            <option value="2122">2021-2022</option>
+            <option value="2021">2020-2021</option>
+          </select>
+          <NavLink to="/predictions">predictions</NavLink>
+          <NavLink to="/boxscores">box scores</NavLink>
         </nav>
       </header>
     </>
