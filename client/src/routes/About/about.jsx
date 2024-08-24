31eb60c225d67f4d19dad997719786dd8ceeaca7
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-08-24 13:25:18 -0500

    test three with bash script

--- client/src/routes/About/about.jsx
+++ client/src/routes/About/about.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function about() {
   return (
     <>
-      <h1>Test2</h1>
+      <h1>Test3</h1>
       <p>Demo project showcasing the use of NodeJs, Express & MongoDB.</p>
       <p>That's pretty much it.</p>
     </>
