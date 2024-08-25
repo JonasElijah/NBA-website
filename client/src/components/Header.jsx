5220c3ce959927c30eb8db01fdd059287fd1f5d8
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-08-25 12:09:06 -0500

    experimenting with select

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,25 +9,24 @@ function Header() {
         <Link to="/" ><h2 className="title">NBA Website</h2></Link>
         <nav>
           <NavLink to="/players">players</NavLink>
-
           <div class="select-wrapper">
             <select name="teams" id="teams">
+              <option value="">teams</option>
               <option value="gsw">Warriors</option>
               <option value="lal">Lakers</option>
               <option value="sa">Spurs</option>
               <option value="dal">Mavericks</option>
             </select>
           </div>
-
           <div class="select-wrapper">
             <select name="seasons" id="seasons">
+              <option value="">seasons</option>
               <option value="2324">2023-2024</option>
               <option value="2223">2022-2023</option>
               <option value="2122">2021-2022</option>
               <option value="2021">2020-2021</option>
             </select>
-          </div>
-          
+          </div>        
           <NavLink to="/predictions">predictions</NavLink>
           <NavLink to="/boxscores">box scores</NavLink>
         </nav>
